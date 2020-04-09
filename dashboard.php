81c5c22e0da386b14df4cdf8be2ed8a853bcc0fe
Akinleye Hephzibah B <fcbah1248@gmail.com>
2020-04-10 00:41:12 +0100

    Uploaded

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,9 @@
-<?php include_once("lib/header.php")?>
+<?php include_once("lib/header.php")
+if(!isset($_SESSION["loggedIn"]) || empty($_SESSION["loggedIn"])){
+    //redirect to our dashboard
+    header("Location: login.php")
+}
+?>
 <h1>Dashboard</h1>
 LoggedIn User ID: <?php echo $_SESSION['loggedIn'];?>
 <?php include_once("lib/footer.php")?>
