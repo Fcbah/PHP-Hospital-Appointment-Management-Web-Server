12f7b6a66937316066e7913662582c7e421391f3
Akinleye Hephzibah B <fcbah1248@gmail.com>
2020-05-06 00:05:27 +0100

    patient can now view transaction

--- view_transaction_history.php
+++ view_transaction_history.php
@@ -1,3 +1,7 @@
 <?php include_once("lib/header.php");
 ?>
+<div class="container">
+    <h1>View Patient Details</h1>
+    <a href="patient.php" class="btn btn-primary">Return to Dashboard</a>
+</div>
 <?php include_once("lib/footer.php")?>
